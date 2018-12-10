30dd408c3cff5d27b151c4599ef63d0bad7ffa15
Elvar Olafsson <b0b4f3tt@gmail.com>
2018-12-10 21:00:16 +0000

    Figured out how to add another element to the gui using the xml

--- src/main/java/elvaro/FXMLController.java
+++ src/main/java/elvaro/FXMLController.java
@@ -1,16 +1,27 @@
 package elvaro;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class FXMLController {
     
     @FXML
     private Label label;
+
+    @FXML
+    private Button button;
     
     public void initialize() {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
-    }    
+        button.setText("test");
+    }
+
+
+    @FXML
+    private void buttonClick() {
+        System.out.println("test");
+    }
 }
