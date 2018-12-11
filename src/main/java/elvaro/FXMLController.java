a1de4d510cac3d22a5c85601f90e1b4db136d6ea
Elvar Olafsson <b0b4f3tt@gmail.com>
2018-12-11 19:48:31 +0000

    Code removal
    Removed most of the code from the controller when I realized that most of the configuration of the ui elements could be done in the fxml file.

--- src/main/java/elvaro/FXMLController.java
+++ src/main/java/elvaro/FXMLController.java
@@ -5,18 +5,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class FXMLController {
-    
-    @FXML
-    private Label label;
 
-    @FXML
-    private Button button;
-    
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
-        button.setText("test");
     }
 
 
